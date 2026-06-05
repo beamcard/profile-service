bd39138d50428873eebdc02229c81a373c19e58a
Yehor Kravchenko <yehor.kravchenko@checkyeti.com>
2026-06-05 21:48:48 +0200

    Create profile-service skeletonwith modules

---
+++ profile-app/src/test/java/com/beamcard/profile/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beamcard.profile;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class ProfileServiceApplicationTests {
+
+    @Test
+    void mainClassIsLoadable() {
+        assertNotNull(ProfileServiceApplication.class);
+    }
+}
